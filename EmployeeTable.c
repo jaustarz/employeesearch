f6eb1364c5a0d9297f8f9ff3902e8ef6690867e4
Jibriel Ustarz <jaustarz@cpp.edu>
2021-10-01 17:33:24 -0700

    Added files

---
+++ EmployeeTable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Employee.h"
+Employee EmployeeTable[] =
+{
+   {1001, "Daphne Borromeo", "909-555-2134", 8.78},
+   {1011, "Tammy Franklin", "213-555-1212", 4.50},
+   {1140, "Dorris Perl", "310-555-1215", 7.80},
+   {4011, "Tony Bobcat", "909-555-1235", 6.34},
+   {5045, "Brian Height", "714-555-2749", 8.32}
+};
+const int EmployeeTableEntries = sizeof(EmployeeTable)/sizeof(EmployeeTable[0]);
